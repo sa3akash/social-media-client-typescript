f4caa6f12f39c98e3b4dceb6cc25b53350e300c7
sa3akash <sa3shakil@gmail.com>
2023-12-17 16:57:51 +0600

    edit user profile

--- src/components/home/items/UserProfile.tsx
+++ src/components/home/items/UserProfile.tsx
@@ -9,17 +9,17 @@ const UserProfile = () => {
 
   return (
     <Link
-      to={`${PageURL.Profile}/123`}
+      to={`${PageURL.Profile}/${user?._id}`}
       className="hidden md:block lg:mt-2 lg:mb-2 lg:px-4 pt-4 pb-4"
     >
       <div className="dark:bg-[#292932] lg:borderWrapper py-3 px-4 lg:rounded-xl flex items-center lg:gap-2 select-none cursor-pointer">
         <UserAvater
           src={user?.profilePicture}
-          name={Utils.getAvaterName(user!.name.first, user!.name.last)}
+          name={Utils.getAvaterName(user!.name?.first, user!.name?.last)}
         />
         <div className="hidden lg:block">
           <h3 className="font-semibold text-[14px] tracking-[0.1px] capitalize">
-            {`${user?.name.first} ${user?.name.last}`}
+            {`${user?.name?.first} ${user?.name?.last}`}
           </h3>
           <span className="text-[#92929D] text-[12px]">{`@${user?.username}`}</span>
         </div>
